edc59c69a4f9e6b82525f14ed659908ec5bdf493
Reeshav Acharya <reeshavacharya@Reeshavs-MacBook-Pro-2.local>
2025-10-02 12:20:43 -0400

    Implement grep

--- grep.c
+++ grep.c
@@ -4,14 +4,41 @@
 #include "stat.h"
 #include "user.h"
 
+int flag_i = 0;
+int flag_n = 0;
+int flag_v = 0;
+
 char buf[1024];
 int match(char*, char*);
+
+char
+lower(char c) {
+  if('A' <= c && c <= 'Z')
+    return c - 'A' + 'a';
+  return c;
+}
+
+void itoa(int n, char *s) {
+  char buf[16];
+  int i = 0, j = 0;
+  if(n == 0){
+    s[0] = '0'; s[1] = '\0';
+    return;
+  }
+  while(n > 0){
+    buf[i++] = (n % 10) + '0';
+    n /= 10;
+  }
+  while(i > 0) s[j++] = buf[--i];
+  s[j] = '\0';
+}
 
 void
 grep(char *pattern, int fd)
 {
-  int n, m;
+  int n, m, lineno = 0;
   char *p, *q;
+  char line[1024];
 
   m = 0;
   while((n = read(fd, buf+m, sizeof(buf)-m-1)) > 0){
@@ -20,7 +47,34 @@ grep(char *pattern, int fd)
     p = buf;
     while((q = strchr(p, '\n')) != 0){
       *q = 0;
-      if(match(pattern, p)){
+      lineno++;
+
+      // Copy line into buffer for case-insensitive comparison
+      strcpy(line, p);
+
+      int matched;
+      if(flag_i){
+        // Lowercase pattern + line
+        char pat[128], lbuf[1024];
+        int i;
+        for(i=0; pattern[i]; i++) pat[i] = lower(pattern[i]);
+        pat[i] = 0;
+        for(i=0; line[i]; i++) lbuf[i] = lower(line[i]);
+        lbuf[i] = 0;
+        matched = match(pat, lbuf);
+      } else {
+        matched = match(pattern, line);
+      }
+
+      if(flag_v) matched = !matched;
+
+      if(matched){
+        if(flag_n){
+          char numbuf[16];
+          itoa(lineno, numbuf);
+          write(1, numbuf, strlen(numbuf));
+          write(1, ":", 1);
+        }
         *q = '\n';
         write(1, p, q+1 - p);
       }
@@ -38,21 +92,37 @@ grep(char *pattern, int fd)
 int
 main(int argc, char *argv[])
 {
-  int fd, i;
+  int fd, i, argi;
   char *pattern;
 
   if(argc <= 1){
-    printf(2, "usage: grep pattern [file ...]\n");
-    exit();
-  }
-  pattern = argv[1];
-
-  if(argc <= 2){
-    grep(pattern, 0);
+    printf(2, "usage: grep [-inv] pattern [file ...]\n");
     exit();
   }
 
-  for(i = 2; i < argc; i++){
+  argi = 1;
+  while(argi < argc && argv[argi][0] == '-'){
+    char *opt = argv[argi];
+    for(int j = 1; opt[j]; j++){
+      if(opt[j] == 'i') flag_i = 1;
+      else if(opt[j] == 'n') flag_n = 1;
+      else if(opt[j] == 'v') flag_v = 1;
+    }
+    argi++;
+  }
+
+  if(argi >= argc){
+    printf(2, "usage: grep [-inv] pattern [file ...]\n");
+    exit();
+  }
+
+  pattern = argv[argi++];
+  if(argi >= argc){
+    grep(pattern, 0); 
+    exit();
+  }
+
+  for(i = argi; i < argc; i++){
     if((fd = open(argv[i], 0)) < 0){
       printf(1, "grep: cannot open %s\n", argv[i]);
       exit();
